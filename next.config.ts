1d146559ce19cdc8b5e26102f5b334deab0755b2
himanshuvkm <himanshuvkm252@gmail.com>
2025-10-24 11:59:50 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+           {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // for Google login images (optional)
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io", // if you show images from Sanity
+      },
+    ],
+  },
+
 };
+
 
 export default nextConfig;
